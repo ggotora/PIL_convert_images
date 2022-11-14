8855e27c0c5ed97c29b55a65feb79cc482b8f0af
gilbertgotora2@gmail.com <gilbertgotora2@gmail.com>
2022-11-14 09:12:27 +0200

    add size and convert mode to RGB

--- changeImage.py
+++ changeImage.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+from PIL import Image
+from pathlib import Path
+
+size = (600, 400)
+paths = Path("supplier-data/images").glob("*.tiff")
+for im_path in paths:
+    outfile = str(im_path.parent) + "/" + str(im_path.stem) + ".jpeg"
+    print(outfile)
+    with Image.open(im_path) as im:
+        if im.mode != "RGD":
+            im = im.convert("RGB")
+            im.thumbnail(size=size)
+            print(im.mode)
+            im.save(outfile)
+# for path in paths:
+#     compress_image(path, path.stem + ".jpg")
